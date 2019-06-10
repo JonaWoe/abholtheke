b66c968f15a2e963e1b1c120111e96d7d7b3f3c4
Daniel Weber <daniel.weber@abas.de>
2019-06-10 14:16:00 +0200

    Moved Prescriptions to Prescriptions Component and cleaned Home Component

--- WebApp/src/app/home/home.component.ts
+++ WebApp/src/app/home/home.component.ts
@@ -1,43 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { first } from 'rxjs/operators';
-
-import { Prescription, User } from '../_models';
-import { PrescriptionService, AuthenticationService } from '../_services';
+import { Component, OnInit } from '@angular/core';
 
 @Component({ templateUrl: 'home.component.html' })
-export class HomeComponent implements OnInit, OnDestroy {
-  currentUser: User;
-  currentUserSubscription: Subscription;
-  prescriptions: Prescription[] = [];
+export class HomeComponent implements OnInit {
 
-  constructor(
-    private authenticationService: AuthenticationService,
-    private prescriptionService: PrescriptionService
-  ) {
-    this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
-      this.currentUser = user;
-    });
-  }
+  constructor() {}
 
   ngOnInit() {
-    this.loadAllPrescriptions();
-  }
-
-  ngOnDestroy() {
-    // unsubscribe to ensure no memory leaks
-    this.currentUserSubscription.unsubscribe();
-  }
-
-  deleteUser(id) {
-    /*this.userService.delete(id).pipe(first()).subscribe(() => {
-      this.loadAllPrescriptions();
-    });*/
-  }
-
-  private loadAllPrescriptions() {
-    this.prescriptionService.getPrescriptionsByInsuranceId(this.currentUser.insuranceId).pipe(first()).subscribe(prescriptions => {
-      this.prescriptions = prescriptions;
-    });
   }
 }
--- WebApp/src/app/prescriptions/prescriptions.component.ts
+++ WebApp/src/app/prescriptions/prescriptions.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
+
+import { Prescription, User } from '../_models';
+import { PrescriptionService, AuthenticationService } from '../_services';
 
 @Component({
   selector: 'app-prescriptions',
@@ -6,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prescriptions.component.css']
 })
 export class PrescriptionsComponent implements OnInit {
+  prescriptions: Prescription[] = [];  
+  currentUser: User;
+  currentUserSubscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private prescriptionService: PrescriptionService
+  ) {
+    this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
+      this.currentUser = user;
+    });
+  }
 
   ngOnInit() {
+    this.loadAllPrescriptions();
+  }
+
+  ngOnDestroy() {
+    // unsubscribe to ensure no memory leaks
+    this.currentUserSubscription.unsubscribe();
+  }
+  
+  deleteUser(id) {
+    /*this.userService.delete(id).pipe(first()).subscribe(() => {
+      this.loadAllPrescriptions();
+    });*/
+  }
+
+  private loadAllPrescriptions() {
+    this.prescriptionService.getPrescriptionsByInsuranceId(this.currentUser.insuranceId).pipe(first()).subscribe(prescriptions => {
+      this.prescriptions = prescriptions;
+    });
   }
 
 }
