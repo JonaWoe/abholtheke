7b384f5379daca949e36cd69e104c392a0314293
Daniel Weber <daniel.weber@abas.de>
2019-06-10 15:27:55 +0200

    Fixed Showing Current User after Login

--- WebApp/src/app/home/home.component.ts
+++ WebApp/src/app/home/home.component.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+
+import { User } from '../_models';
+import { AuthenticationService } from '../_services';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent implements OnInit {
+  currentUser: User;
+  currentUserSubscription: Subscription;
 
-  constructor() {}
+  constructor(
+    private authenticationService: AuthenticationService) 
+    {
+      this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
+        this.currentUser = user;
+      })
+    }
 
   ngOnInit() {
   }
