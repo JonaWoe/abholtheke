df938616f7198749206e502d2bc863a2052e60ee
jonawoe <jonawoessner@gmail.com>
2019-06-25 16:09:52 +0200

    lead boxes in pharmacist home component

--- WebApp/src/app/pharmacist-home/pharmacist-home.component.ts
+++ WebApp/src/app/pharmacist-home/pharmacist-home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+import { Box, Pharmacist } from '../_models';
+import { AuthenticationService, BoxService } from '../_services';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pharmacy',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PharmacistHomeComponent implements OnInit {
 
-  constructor() { }
+  currentUser: Pharmacist;
+  currentUserSubscription: Subscription;
+  boxes: Box[] = [];
+
+  constructor(private boxService: BoxService, private authenticationService: AuthenticationService,) {
+    this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
+      this.currentUser = (user as unknown as Pharmacist);
+    });
+  }
 
   ngOnInit() {
+    this.loadAllBoxes();
+  }
+
+  private loadAllBoxes() {
+    this.boxService.getBoxesByPharmacyId(this.currentUser.pharmacyId).pipe(first()).subscribe(boxes => {
+      this.boxes = boxes;
+    });
   }
 
 }
